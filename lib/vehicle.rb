40cf5ab68b55e315f5ddb3a231b20a23e5dd724e
Tommy Connor <tconnor98@aol.com>
2020-02-20 15:53:10 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,12 @@
 class Vehicle
+  attr_accessor :name, :wheel_size, :number
   
+  def initialize(name, wheel_size, number)
+    @name = name
+    @wheel_size = wheel_size
+    @number = number
+  end
+  
+  def go 
+    "vrrrr"
 end
